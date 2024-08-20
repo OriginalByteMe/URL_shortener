97a040c3fbadc29f3147faafa8fa9d33912ee888
OriginalByteMe <noahrijkaard@gmail.com>
2024-08-20 22:46:22 +0800

    Fix bad migration

--- db/migrate/20240820100550_remove_clicks_from_user_stats.rb
+++ db/migrate/20240820100550_remove_clicks_from_user_stats.rb
@@ -4,7 +4,5 @@ class RemoveClicksFromUserStats < ActiveRecord::Migration[7.2]
     add_column :user_stats, :timezone, :string
     add_column :user_stats, :latitude, :float
     add_column :user_stats, :longitude, :float
-
-    add_index :table, [:latitude, :longitude]
   end
 end
